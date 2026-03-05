0a8be146f66620ac2a936cf00e83d07663f93d00
JK-22025 <kazanj7@gmail.com>
2026-03-04 21:44:47 -0500

    My Work

---
+++ FootyHub/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  FootyHub
+//
+//  Created by Jawad Kazan on 2026-03-04.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
